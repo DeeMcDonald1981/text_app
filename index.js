0e6642c010806780b2d57bcb5666aaaf01c571d0
virgoEnigma1981 <dee.mcdonald33@gmail.com>
2023-09-23 12:11:34 -0500

    Updated the index.js file to locate the css file & and to log the message from index.html form

--- index.js
+++ index.js
@@ -3,45 +3,83 @@ import http from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import finalhandler from 'finalhandler'
-import serverStatic from 'serve-static'
 
-let serve = serverStatic("public");
-const server = http.createServer((req, res)=>{
-  if(req.method === 'GET' && req.url === '/'){
-    let currentFile = import.meta.url;
-    let __fileanme = fileURLToPath(currentFile);
-    let __dirname = dirname(__fileanme);
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  // Get the current file's URL and directory path
+  const currentFileURL = import.meta.url
+  let __filename = fileURLToPath(currentFileURL);
+  let __dirname = dirname(__filename);
+  // Handle GET requests
+  if (req.method === 'GET') {
+    let filePath;
 
-    const filePath = path.join(__dirname, 'public', 'index.html')
+    // If the request is for the root URL, serve index.html
+    if (req.url === '/') {
+      filePath = path.join(__dirname, 'public', 'index.html');
+    } else {
+      // Otherwise, serve the file specified in the URL
+      filePath = path.join(__dirname, 'public', req.url);
 
-    const readStream = fs.createReadStream(filePath);
+    // Check if the requested file is 'favicon.ico'
+      if (req.url === '/favicon.ico') {
+      // Ignore this request and do not attempt to read the file
+      res.statusCode = 204; // No content
+      res.end();
+      return; // Exit the function to prevent further execution
+      }
+      
+    }
 
-    readStream.pipe(res)
-  }else if(req.method === 'POST' && req.url === '/submit'){
+    // Create a readable stream to read the file
+    const readStream = fs.createReadStream(filePath, 'utf8');
+
+
+    // Handle errors during file reading
+    readStream.on('error', (error) => {
+      console.error('Error reading file:', error);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    });
+
+    // Send the file's contents to the client
+    readStream.pipe(res);
+  }
+  
+  // Check if the incoming HTTP request is a POST request to the '/submit' URL
+  else if (req.method === 'POST' && req.url === '/submit') {
+    // Initialize an empty string to store the request body data
     let body = '';
-    req.on('data', (chunk)=>{
+
+    // Listen for data events on the request object (as data arrives from the client)
+    req.on('data', (chunk) => {
+      // Concatenate each chunk of data to the 'body' string
       body += chunk;
     });
 
-    req.on('end', ()=>{
+    // Listen for the end event on the request object (when all data has been received)
+    req.on('end', () => {
+      // Decode the received data (assuming it's URL-encoded) into a string
       const formData = decodeURIComponent(body);
+
+      // Split the formData by '=' to extract the user input value
       const userInput = formData.split('=')[1];
 
+      // Log the user input to the console
       console.log('User input:', userInput);
-      res.end('Data recieved!')
+
+      // Send a response to the client indicating that data has been received
+      res.end('Data received!');
     });
-
-
-  }else{
-    res.statusCode = 404;
-    res.end('Not found')
   }
-  
+    // Handle all other requests with a 404 Not Found response
+  else {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
 });
 
-server.listen(3000, (req,res)=>{
-  let done = finalhandler(req,res);
-  serve(req, res, done)
+// Start the server on port 3000
+server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
